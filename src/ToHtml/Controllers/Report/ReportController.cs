3a669a0c01d80928f00c69959506a2897800cacf
wk <somnuk.wk@gmail.com>
2020-03-08 00:19:40 +0700

    Split

--- src/ToHtml/Controllers/Report/ReportController.cs
+++ src/ToHtml/Controllers/Report/ReportController.cs
@@ -1,18 +1,14 @@
-using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System.Linq;
 using ToHtml.Extensions;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc.ViewEngines;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace ToHtml.Controllers.Report {
 
@@ -38,37 +34,3 @@ namespace ToHtml.Controllers.Report {
     }
 }
 
-
-namespace ToHtml.Extensions {
-    public static class ViewResultExtensions {
-        public static async Task<string> RenderViewAsync<TModel>(this Controller controller, string viewName, TModel model, bool partial = false) {
-            if (string.IsNullOrEmpty(viewName)) {
-                viewName = controller.ControllerContext.ActionDescriptor.ActionName;
-            }
-
-            controller.ViewData.Model = model;
-
-            using (var writer = new StringWriter()) {
-                IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
-                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, !partial);
-
-                if (viewResult.Success == false) {
-                    return $"A view with the name {viewName} could not be found";
-                }
-
-                ViewContext viewContext = new ViewContext(
-                    controller.ControllerContext,
-                    viewResult.View,
-                    controller.ViewData,
-                    controller.TempData,
-                    writer,
-                    new HtmlHelperOptions()
-                );
-
-                await viewResult.View.RenderAsync(viewContext);
-
-                return writer.GetStringBuilder().ToString();
-            }
-        }
-    }
-}
