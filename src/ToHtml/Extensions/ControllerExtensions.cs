eac1d0c1e85896bff0106d2ecc07c1ea457b3960
wk <somnuk.wk@gmail.com>
2020-08-21 19:57:09 +0700

    Refactor

--- src/ToHtml/Extensions/ControllerExtensions.cs
+++ src/ToHtml/Extensions/ControllerExtensions.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.ViewEngines;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
-
 namespace ToHtml.Extensions {
     public static class ViewResultExtensions {
         public static async Task<string> RenderViewAsync<TModel>(this Controller controller, string viewName, TModel model, bool partial = false) {
@@ -15,27 +14,28 @@ namespace ToHtml.Extensions {
 
             controller.ViewData.Model = model;
 
-            using (var writer = new StringWriter()) {
-                IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
-                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, !partial);
+            using var writer = new StringWriter();
+            IViewEngine viewEngine = controller.HttpContext.RequestServices
+                .GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
 
-                if (viewResult.Success == false) {
-                    return $"A view with the name {viewName} could not be found";
-                }
+            ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, !partial);
 
-                ViewContext viewContext = new ViewContext(
-                    controller.ControllerContext,
-                    viewResult.View,
-                    controller.ViewData,
-                    controller.TempData,
-                    writer,
-                    new HtmlHelperOptions()
-                );
-
-                await viewResult.View.RenderAsync(viewContext);
-
-                return writer.GetStringBuilder().ToString();
+            if (!viewResult.Success) {
+                return $"A view with the name {viewName} could not be found";
             }
+
+            ViewContext viewContext = new ViewContext(
+                controller.ControllerContext,
+                viewResult.View,
+                controller.ViewData,
+                controller.TempData,
+                writer,
+                new HtmlHelperOptions()
+            );
+
+            await viewResult.View.RenderAsync(viewContext);
+
+            return writer.GetStringBuilder().ToString();
         }
     }
 }
